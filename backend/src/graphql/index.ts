5418329adf1cdbfa6bc1bcaf1c91aea8fd3d9003
Sachin Gupta <saching@microsoft.com>
2019-06-03 00:10:57 -0700

    addign User Model and connect event to user model

--- backend/src/graphql/index.ts
+++ backend/src/graphql/index.ts
@@ -1,5 +1,7 @@
 import { buildSchema } from "graphql";
 import { EventModel } from "../models/event";
+import { UserModel } from "../models/user";
+import * as bcrypt from "bcryptjs";
 
 export const RootSchema = buildSchema(`
   type Event {
@@ -8,6 +10,17 @@ export const RootSchema = buildSchema(`
       description: String
       price: Float
       date: String
+  }
+
+  type User {
+      _id: ID!
+      email: String!
+      password: String
+  }
+
+  input UserInput {
+      email: String!
+      password: String!
   }
 
   input EventInput {
@@ -22,39 +35,77 @@ export const RootSchema = buildSchema(`
 
   type Mutation {
       createEvent(eventInput: EventInput): Event
+      createUser(userInput: UserInput): User
   }
 `);
 
 
 export const RootValue = {
-  events: function () {
-    return EventModel.find()
-    .then((events: any) => {
-      console.log(events);
-      return events.map((event: any) => {
-        return { ...event._doc, _id: event._doc._id.toString() };
-      });
-    })
-    .catch((err: any) => {
-      console.log(err);
-    });
-  },
+    events: function () {
+        return EventModel.find()
+            .then((events: any) => {
+                console.log(events);
+                return events.map((event: any) => {
+                    return { ...event._doc, _id: event._doc._id.toString() };
+                });
+            })
+            .catch((err: any) => {
+                console.log(err);
+            });
+    },
 
-  createEvent: function ({ eventInput }: any) {
-    const event = new EventModel({
-      title: eventInput.title,
-      description: eventInput.description,
-      price: eventInput.price,
-      date: new Date()
-    });
+    createEvent: function ({ eventInput }: any) {
+        const event = new EventModel({
+            title: eventInput.title,
+            description: eventInput.description,
+            price: eventInput.price,
+            date: new Date(),
+            creator: '5cf4c3e41321f9489cb223a0'
+        });
 
-    return event.save()
-      .then((result: any) => {
-        console.log(result);
-        return { ...result._doc };
-      })
-      .catch((err: any) => {
-        console.log(err);
-      });
-  }
+        let createdEvent: any;
+        return event.save()
+            .then((result: any) => {
+                createdEvent = { ...result._doc };
+                return UserModel.findById('5cf4c3e41321f9489cb223a0')
+            })
+            .then((user: any) => {
+                if (!user) {
+                    throw new Error("User doesn't exists");
+                }
+                user.createdEvents.push(event);
+            })
+            .then((result: any) => {
+                return createdEvent;
+            })
+            .catch((err: any) => {
+                console.log(err);
+            });
+    },
+
+    createUser: function ({ userInput }: any) {
+        return UserModel.findOne({ email: userInput.email })
+            .then((user: any) => {
+                if (user) {
+                    throw new Error("User exists already");
+                }
+                return bcrypt.hash(userInput.password, 12)
+                    .then((hasbPassword) => {
+                        const user = new UserModel({
+                            email: userInput.email,
+                            password: hasbPassword
+                        });
+                        return user.save()
+                            .then((result: any) => {
+                                return { ...result._doc };
+                            })
+                            .catch((err: any) => {
+                                console.log(err);
+                            });
+                    })
+                    .catch((err: any) => {
+                        console.log(err);
+                    });
+            });
+    }
 };
---
+++ backend/src/models/user.ts
@@ -0,0 +1,22 @@
+import * as mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    createdEvents: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Event'
+        }
+    ]
+});
+
+export const UserModel = mongoose.model('User', userSchema);
